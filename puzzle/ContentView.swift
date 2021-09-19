cf4032b876cc23f9322b5ce89850d321e562cc36
Nickolay Trukhin <coolone.official@gmail.com>
2021-09-19 18:12:19 +0300

    Initial Commit

---
+++ puzzle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  puzzle
+//
+//  Created by Nickolay Truhin on 19.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ puzzle/puzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  puzzleApp.swift
+//  puzzle
+//
+//  Created by Nickolay Truhin on 19.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct puzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
